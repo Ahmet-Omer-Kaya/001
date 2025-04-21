74a727bf632b45cbc584dd8e6c91a139a7dc3e04
Ahmet Ömer Kaya <kayaomerahmet55@gmail.com>
2025-04-21 07:10:30 +0300

    cin & getline ile biyografi

---
+++ biyografi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string isim;
+	string yemek;
+	string flim;
+	
+	cout << "---Biyografi---" << endl;
+	cout << "Adý-Soyadý: " << endl;
+	getline(cin,isim);
+	cout << "En sevdigi Yemek: " << endl;
+	getline(cin,yemek);
+	cout << "En Sevditgi Flim: " << endl;
+	getline(cin,flim);
+	cout << "---Biyografi---" << endl;
+}
